8266856b024f51501cb31b6d7b3542aa97b83634
Nikita Sasunkevich <nikita@Nikitas-MacBook-Pro.local>
2017-12-12 00:00:15 -0700

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAfREUsdLVvhGMpi7V7EHn8urz1VxTEd7w",
+    authDomain: "trainschedulerproject.firebaseapp.com",
+    databaseURL: "https://trainschedulerproject.firebaseio.com",
+    projectId: "trainschedulerproject",
+    storageBucket: "trainschedulerproject.appspot.com",
+    messagingSenderId: "340145154188"
+};
+firebase.initializeApp(config);
+
